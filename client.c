55608c6c249951694c6ccf9f7631302845d5a672
Sofiane <sof2611@gmail.com>
2016-11-29 08:33:36 +0100

    Connection Serveur Fork <> clients

--- client.c
+++ client.c
@@ -4,15 +4,17 @@
 #include <sys/unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 #include <stdlib.h>
 #include <errno.h>
 
 #define MAX 500
-int main(int argc, char *argv[]){
+/*int main(int argc, char *argv[]){
 
 //  int client;  
   int i;
-  while(i){
+  i = 1;
+  while(1){
   i = socketClient("localhost", 9000, TCP);
   char message[MAX];
   fgets(message, MAX, stdin);
@@ -22,4 +24,29 @@ int main(int argc, char *argv[]){
   i++;
   }
   return 0;
+}*/
+int main(int argc, char *argv[]){
+	int n;
+	//int sockfd = socketClient("localhost", 9000, TCP);
+   char buffer[256];
+	while(1){
+	int sockfd = socketClient("localhost", 9000, TCP);
+	printf("chaine à envoyer: ");
+	bzero(buffer,256);
+	fgets(buffer,255,stdin);
+	connect(sockfd, NULL, 0);
+	n = write(sockfd,buffer,strlen(buffer));
+	if (n < 0){
+		perror("ERREUR ecriture socket");
+	}
+	bzero(buffer,256);
+	n = read(sockfd,buffer,255);
+	if (n < 0){
+		perror("ERREUR lecture socket");
+	}
+	printf("%s\n",buffer);
+	close(sockfd);
+	
+	}
+	return 0;
 }
--- serveur.c
+++ serveur.c
@@ -4,31 +4,59 @@
 #include <sys/unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 #include <stdlib.h>
 #include <errno.h>
 
+void lecture (int );
 int main(int argc, char *argv[]){
 
-  int mes;
+  //int mes;
   int num;
-  pid_t child_serv;
-  int tube_in[2];
-  int tube_out[2];
-  int tube[argv[3]][2];
-  pipe(tube_in);
-  pipe(tube_out);
+  int newsockfd;
+  pid_t pid;
   num = socketServer(9000,TCP);
   afficheInfosSocket(num);
 
-  for(((child_serv = fork()) == 0){
-  dup2(tube[0], STDIN_FILENO);
-  close(tube[0]);
+	/*while(1){
   mes = accept(num, NULL, NULL);
   char message[100];
   int nb;
   nb = read(mes, message, 100);
   printf("%d\n",nb);
   printf("%s\n",message);
-  }
-return 0;
+  }*/
+	while (1) {
+		newsockfd = accept(num, NULL, NULL);
+		if (newsockfd < 0){ 
+			perror("ERREUR accept");
+		}
+		pid = fork();
+		if (pid < 0){
+			perror("ERREUR du fork");
+		}
+		if (pid == 0)  {
+			close(num);
+			lecture(newsockfd);
+			exit(0);
+		}
+		else{
+			close(newsockfd);
+		}
+	}
+
+	return 0;
+}
+
+void lecture (int sock)
+{
+   int n;
+   char buffer[256];
+      
+   bzero(buffer,256);
+   n = read(sock,buffer,255);
+   if (n < 0) perror("ERREUR lecture socket");
+   printf("Message: %s\n",buffer);
+   n = write(sock,"Bien recu",9);
+   if (n < 0) perror("ERREUR ecriture socket");
 }
